f7a107bd3e8ad9cf9b2dfcdcc83269d074d879b5
Samir Nijenhuis <samirnijenhuis@gmail.com>
2018-01-10 16:43:31 +0100

    Remove unused class from app.php.

--- routes/web.php
+++ routes/web.php
@@ -14,46 +14,9 @@
 Route::get('/', function(){
     \Artisan::call('image:capture', ['name' => 'test-samir-asd']);
 });
-Route::get('/a', function () {
-    $a = [
-        [
-            "dimension" => "1280x768",
-            "filename" => "homepage",
-            "before"     => ".eyes/before/1280x768/homepage.png",
-            "after"      => ".eyes/after/1280x768/homepage.png",
-            "difference" => ".eyes/diff_before_after/1280x768/homepage.png",
-            "percentage" => 0.0028636163785533,
-        ],
-        [
-            "dimension" => "1920x1080",
-            "filename" => "homepage",
-            "before"     => ".eyes/before/1920x1080/homepage.png",
-            "after"      => ".eyes/after/1920x1080/homepage.png",
-            "difference" => ".eyes/diff_before_after/1920x1080/homepage.png",
-            "percentage" => 0.0018522167638461,
-        ],
-        [
-            "dimension" => "320x480",
-            "filename" => "homepage",
-            "before"     => ".eyes/before/320x480/homepage.png",
-            "after"      => ".eyes/after/320x480/homepage.png",
-            "difference" => ".eyes/diff_before_after/320x480/homepage.png",
-            "percentage" => 0.004484595439981,
-        ],
-        [
-            "dimension" => "320x480",
-            "filename" => "blabla",
-            "before"     => ".eyes/before/320x480/homepage.png",
-            "after"      => ".eyes/after/320x480/homepage.png",
-            "difference" => ".eyes/diff_before_after/320x480/homepage.png",
-            "percentage" => 0.004484595439981,
-        ]
-
-    ];
-
-    return
-        collect($a)->groupBy('filename')->transform(function($value, $key){
-            return
-                ["title" => $key, 'resolutions' => collect($value)->keyBy('dimension')->sort(), 'selected' => $value->first()['dimension']];
-        })->values()->toArray();
+Route::get('/diff/{baseline}/{current}', function ($baseline, $current) {
+   $view = file_get_contents(
+       storage_path("app/.eyes/diff_{$baseline}_{$current}/output.html")
+   );
+   return $view;
 });
