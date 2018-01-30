56932f98d4e224b7edadc433803d8eb8d4999c91
Samir Nijenhuis <samirnijenhuis@gmail.com>
2018-01-30 11:16:03 +0100

    Implement Repositories.

--- app/Browsers/Traits/Capture.php
+++ app/Browsers/Traits/Capture.php
@@ -2,6 +2,7 @@
 
 namespace App\Browsers\Traits;
 
+use App\Repositories\PageRepository;
 use Laravel\Dusk\Browser;
 
 trait Capture {
@@ -17,34 +18,25 @@ trait Capture {
      */
     public function capture($group, $page, $size)
     {
+
         $this->group = $group;
         $this->name = $page['name'];
 
         $this->setUp();
 
-        $page['ignore'] = collect((array) $page['ignore'])->map(function($component){
-            return "var elements = document.querySelectorAll('{$component}'); for(var i = 0; i < elements.length; i++) {elements[i].style.display = 'none';}";
-        })->all();
 
-
-        $page['scripts'] = collect((array) $page['scripts'])->map(function($script){
-            // If it's a local file, search from the base dir.
-            if(! starts_with($script, ['http', 'https'])) {
-                $script = base_path($script);
-            }
-
-            return file_get_contents($script);
-        })->all();
+        $page = new PageRepository($page);
 
         $this->browse(function (Browser $browser)  use($size, $page) {
-            list($width, $height) = explode('x', $size, 2);
+            $browser->visit($page->url)
+                ->resize($page->width(), $page->height());
 
-            $browser->visit($page['url'])
-                ->resize(intval($width), intval($height))
-                ->pause($page['wait-for-delay']);
+            $browser->script($page->scripts());
 
-            $browser->script($page['ignore']);
-            $browser->script($page['scripts']);
+            $browser->pause($page->waitForDelay);
+
+            $browser->script($page->ignore());
+
 
             if( ! file_exists($browser::$storeScreenshotsAt . '/' . $size)) {
                 mkdir($browser::$storeScreenshotsAt . '/' . $size);
---
+++ app/Repositories/PageRepository.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Repositories;
+
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+
+class PageRepository implements Arrayable, Jsonable
+{
+    protected $page;
+
+    public function __construct($page)
+    {
+        $this->page = collect($page);
+    }
+
+    /**
+     * Generate a JS script that hides/ignores the given component.
+     *
+     * @return array
+     */
+    public function ignore()
+    {
+        return collect((array) $this->page->get('ignore', []))->map(function($component){
+            return "var elements = document.querySelectorAll('{$component}'); for(var i = 0; i < elements.length; i++) {elements[i].style.display = 'none';}";
+        })->all();
+    }
+
+    /**
+     * Loads scripts.
+     *
+     * @return array
+     */
+    public function scripts()
+    {
+        return collect((array) $this->page->get('scripts', []))->map(function($script){
+            // If it's a local file, search from the base dir.
+            if(! str_contains($script, '://') ) {
+                $script = base_path($script);
+            }
+            return file_get_contents($script);
+        })->all();
+    }
+
+    /**
+     * Grabs the given width.
+     *
+     * @return int
+     */
+    public function width()
+    {
+        list($width, $height) = explode('x', $this->page->get('size'), 2);
+        return (int) $width;
+    }
+
+    /**
+     * Grabs the given height.
+     *
+     * @return int
+     */
+    public function height()
+    {
+        list($width, $height) = explode('x', $this->page->get('size'), 2);
+        return (int) $height;
+    }
+
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->page->toArray();
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int $options
+     *
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return $this->page->toJson($options);
+    }
+
+
+    /**
+     * Grabs the $page key first by kebabcase, then by given case.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->page->get(
+            kebab_case($key),
+            $this->page->get($key)
+        );
+    }
+
+}
